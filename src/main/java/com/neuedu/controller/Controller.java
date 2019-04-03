c9df0976c7587203bb7f10da8461b4392d8a0264
Urara <435702754@qq.com>
2019-04-03 10:29:15 +0800

    添加ssm框架

--- src/main/java/com/neuedu/controller/Controller.java
+++
@@ -1,7 +0,0 @@
-package com.neuedu.controller;
-
-/**
- * Created by Urara on 2019/3/28.
- */
-public class Controller {
-}
---
+++ src/test/java/com/neuedu/test/AutoCode.java
@@ -0,0 +1,18 @@
+package com.neuedu.test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Urara on 2019/4/1.
+ */
+public class AutoCode {
+    public static void main(String[] args){
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        //指定逆向工程配置文件
+        File configFile = new File(System.getProperty("user"));
+    }
+}
